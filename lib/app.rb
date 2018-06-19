83e3094fae3f25c68330917f9f766ea061854a33
Raefe <raefe.nj@gmail.com>
2018-06-19 20:44:33 +0100

    added new recipient button and minor refactoring

--- lib/app.rb
+++ lib/app.rb
@@ -5,9 +5,13 @@ class Fakebook < Sinatra::Base
 
   get '/' do
     api = Api.new
-    api.authentication_request
+    @token = api.authentication_request
     @recipients = api.recipient_list
     erb(:index)
+  end
+
+  post '/new-recipient' do
+    redirect('/')
   end
 
   run! if app_file == $0
